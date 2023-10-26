ae17af5e4b17870b3955cb03d569713a91833ed1
unknown <denis.hadzipasic@partyrent.com>
2023-10-26 17:04:52 +0200

    last change

---
+++ hackerNews/src/components/Footer.jsx
@@ -0,0 +1,6 @@
+import { Spinner, Pagination, Heading, Pane, Link, Text } from 'evergreen-ui'
+
+export default function Footer() {
+
+      <Pagination page={page} totalPages={5} onPageChange={handlePage} onNextPage={handleNextPage} onPreviousPage={handlePreviousPage}></Pagination>
+}
