926d57b5684c7091c2cf192892e83e7d383a9396
Pia <pia.koester@hey.com>
2023-10-26 21:25:46 +0200

    fix: added prevent Default to the handleInput function

--- hackerNews/src/components/News.jsx
+++ hackerNews/src/components/News.jsx
@@ -7,17 +7,18 @@ import { Spinner, Pagination, Heading, Pane, Link, Text } from 'evergreen-ui'
 export default function News() {
   const [articles, setArticles] = useState(null);
   const [search, setSearch] = useState(null);
-  const [page, setPage] = useState(1)
-  const url = "https://hn.algolia.com/api/v1/search?query="
+  const [inputSearch, setInputSearch] = useState(null);
+  const [page, setPage] = useState(1);
+  const url = "https://hn.algolia.com/api/v1/search?query=";
 
   useEffect(() => {
     getData();
-  }, []); // brauchen wir nur initial ein einziges mal und nicht bei jedem State wechsel
+  }, [inputSearch, page]); // every time the State of these changes the page is refreshed
 
   const getData = async () => {
     try {
       const response = await axios.get(
-        `${url}${search}&page=${page}`
+        `${url}${inputSearch}&page=${page}` // request URL is based on the search parameter and the page Number
       );
       setArticles(response.data.hits);
     //   console.log(response.data.hits)
@@ -27,32 +28,26 @@ export default function News() {
   };
 
   const handleChange = (event) => {
-    setSearch(event.target.value)
+    setSearch(event.target.value) // whatever is written in the input gets set as the new search value
     // console.log(search)
   }
 
   const handleInput = (e) => {
     e.preventDefault(); // verhindert dass die Seite neu geladen wird, damit wir die Ergebnisse sehen
-    // setInputSearch(search) brauchen wir nicht mehr, weil jetzt in der handleInput function die getData func genutzt wird
-    console.log(search)
-    console.log(page)
-    setPage(1); 
-    getData();
+    setInputSearch(search); // the inputSearch which is send as a request is updated to the final text in the input
+    setPage(1);  // page is set to 1 because after each new search we want to start at 1
   }
 
   const handlePage = (page) => {
-    setPage(page)
-    getData();
+    setPage(page) // page is a parameter here which is based on the function in the pagination
   }
 
   const handleNextPage = () => {
-    setPage((prevPage) => prevPage + 1);
-    getData();
+    setPage((prevPage) => prevPage + 1); // this takes the previous page value and adds one to it
   }
 
   const handlePreviousPage = () => {
     setPage((prevPage) => prevPage - 1);
-    getData();
   }
 
   return (
